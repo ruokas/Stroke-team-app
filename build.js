8b0cc255a79735cdcadb796da119e0f7885527e8
Rokas M <63348397+ruokas@users.noreply.github.com>
2025-08-27 10:40:52 +0300

    chore: add autoprefixer and optimize build

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ import fs from 'fs/promises';
 import nunjucks from 'nunjucks';
 import postcss from 'postcss';
 import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
 
 nunjucks.configure('templates', { autoescape: false });
 
@@ -13,13 +14,12 @@ async function buildHtml() {
 async function buildCss() {
   const files = ['css/layout.css', 'css/components.css', 'css/forms.css'];
   const contents = await Promise.all(files.map(f => fs.readFile(f, 'utf8')));
-  const result = await postcss([cssnano]).process(contents.join('\n'), { from: undefined });
+  const result = await postcss([autoprefixer, cssnano]).process(contents.join('\n'), { from: undefined });
   await fs.writeFile('css/style.css', result.css);
 }
 
 try {
-  await buildHtml();
-  await buildCss();
+  await Promise.all([buildHtml(), buildCss()]);
 } catch (error) {
   console.error('Build failed:', error);
   process.exit(1);
